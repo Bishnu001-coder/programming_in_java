6bdc6a0c77c52633bb04a5daf20a733c607d2054
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-09-01 22:53:46 +0530

    solved LCM_HCF problem

---
+++ src/Number_programming_question/LCM_and_HCF.java
@@ -0,0 +1,23 @@
+package Number_programming_question;
+
+import java.util.Scanner;
+
+public class LCM_and_HCF {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter two number");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int hcf=1;
+        //this for hcf
+        for(int i=1;i<=a && i<=b;i++){
+            if(a%i==0 && b%i==0){
+                hcf=i;
+            }
+        }
+        System.out.println("hcf value is = "+hcf);
+        //this for the lcm
+        int lcm=(a*b)/hcf;
+        System.out.println("the lcm value is  = "+lcm);
+    }
+}
