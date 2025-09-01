87a38d5d823036a4dec59a17a9ef2ef5e76b525a
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-09-01 18:46:32 +0530

    first java program

---
+++ src/Number_programming_question/pallindrome.java
@@ -0,0 +1,18 @@
+package Number_programming_question;
+
+public class pallindrome {
+    public static void main(String[] args) {
+        int org_num = 141;
+        int num = org_num;
+        int rev=0;
+        while(num>0){
+            rev=rev*10+(num%10);
+            num/=10;
+        }
+        if(rev==org_num){
+            System.out.println("it is a pallindrome");
+        }else{
+            System.out.println("it is not a pallindrome");
+        }
+    }
+}
