b4c068cf9de7bd54111d69c6f3ab1e5a980b258a
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-09-01 23:45:13 +0530

    odd_even problem solved

---
+++ src/Number_programming_question/odd_even.java
@@ -0,0 +1,24 @@
+package Number_programming_question;
+import java.util.Scanner;
+public class odd_even {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number ");
+        int num=sc.nextInt();
+        System.out.println("all the even number");
+        for(int i=2;i<num;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+        System.out.println("all the odd number");
+        for(int i=2;i<=num;i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
+
+
+
+}
