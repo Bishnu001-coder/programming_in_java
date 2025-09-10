0f6ed210395ec1dde126caaf9b6c760946113b86
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-09-10 13:21:16 +0530

    remove spaces solved

---
+++ src/STRING/_4_remove_all_spaces_string.java
@@ -0,0 +1,17 @@
+package STRING;
+
+public class _4_remove_all_spaces_string {
+    public static void main(String[] args) {
+        String s="bishnu prasad maharana";
+        String res="";
+        char ch[]=s.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            if(ch[i] !=' '){
+                res+=ch[i];
+            }
+        }
+        System.out.println(ch);
+
+
+    }
+}
