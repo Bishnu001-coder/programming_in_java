9b47092afeb603c42745157d5e20969f74ffeb1a
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-09-10 14:27:43 +0530

    pallindrom String

---
+++ src/STRING/string_pallindrome.java
@@ -0,0 +1,19 @@
+package STRING;
+
+public class string_pallindrome {
+    public static void main(String[] args) {
+        String s="madam";
+        String rev="";
+        for(int i=s.length()-1;i>=0;i--){
+            char ch=s.charAt(i);
+               rev+=ch;
+        }
+
+        if(rev.equals(s)){
+            System.out.println("it is a pllaindrome");
+
+        }else{
+            System.out.println("it is not a pallindrome");
+        }
+    }
+}
