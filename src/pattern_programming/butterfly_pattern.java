226359ccaad466ee9191ed4f60595feeab4c04bf
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-09-10 12:38:24 +0530

    this is the first program in string

---
+++ src/pattern_programming/butterfly_pattern.java
@@ -0,0 +1,37 @@
+package pattern_programming;
+
+import java.util.Scanner;
+
+public class butterfly_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = sc.nextInt();
+
+            for (int i = 1; i <= num; i++) {
+            for (int st = 1; st <= i; st++) {
+                System.out.print("*");
+            }
+            for (int sp = 1; sp <= 2 * (num - i); sp++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = num-1; i >=1; i--) {
+            for (int st = 1; st <= i; st++) {
+                System.out.print("*");
+            }
+            for (int sp = 1; sp <= 2 * (num - i); sp++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
