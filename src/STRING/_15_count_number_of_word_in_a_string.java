9ff6a0df6ee3a3ff0e67851a74bd50ce1a94f8d6
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-09-11 00:02:18 +0530

    solved the the number words in a string

---
+++ src/STRING/_15_count_number_of_word_in_a_string.java
@@ -0,0 +1,22 @@
+package STRING;
+
+public class _15_count_number_of_word_in_a_string {
+    public static void main(String[] args){
+    String s="bishnu prasad maharana";
+    String str="";
+    int i=0,j=0, word=0;
+    while(j<s.length()){
+        while(j<s.length() && s.charAt(j) != ' ') {
+            str+=s.charAt(j);
+            j++;
+        }
+        word=j-i;
+        System.out.println(str+"->"+word);
+        str="";
+       j++;
+       i=j;
+
+    }
+    }
+
+}
