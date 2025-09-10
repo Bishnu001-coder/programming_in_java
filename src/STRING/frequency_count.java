6e0704f7c39e38e6b0a8dcc1fb5bb778ed722b37
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-09-10 14:55:37 +0530

    freqency count of the string

---
+++ src/STRING/frequency_count.java
@@ -0,0 +1,17 @@
+package STRING;
+
+public class frequency_count {
+    public static void main(String[] args) {
+        String s="bishnu prasad maharana";
+            s=s.replace(" ", "");
+            int feq[]=new int[256];
+            for(int i=0;i<s.length();i++){
+                feq[s.charAt(i)]++;
+            }
+            for(int i=0;i<256;i++){
+                if(feq[i]>0){
+                    System.out.println((char)i +"->"+feq[i]);
+                }
+            }
+    }
+}
