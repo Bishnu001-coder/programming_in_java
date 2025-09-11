8297fdc13396d3107cce449c99383f81fde365dc
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-09-11 22:45:49 +0530

    important problem solved

---
+++ src/STRING/_19_sort_a_string.java
@@ -0,0 +1,21 @@
+package STRING;
+
+public class _19_sort_a_string {
+    public static void main(String[] args) {
+        String s="aeroplane";
+        char[] ch=s.toCharArray();
+        for(int i=0;i<s.length();i++){
+
+            for(int j=i+1;j<s.length();j++){
+                if(ch[j]==' ')continue;
+                if(ch[i]>ch[j]){
+                    char temp=ch[i];
+                    ch[i]=ch[j];
+                    ch[j]=temp;
+                }
+
+            }
+            System.out.print(ch[i]+" ");
+        }
+    }
+}
---
+++ src/STRING/_20_replace_repeated_char_with_count_num.java
@@ -0,0 +1,34 @@
+package STRING;
+
+public class _20_replace_repeated_char_with_count_num {
+    public static void main(String[] args) {
+        String s="hello world";
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch==' ')continue;
+            int totalcount=0;
+
+            for(int j=0;j<n;j++){
+                if(s.charAt(j)==ch){
+                    totalcount++;
+                }
+            }
+            if(totalcount==1){
+                System.out.print(ch);
+            }else{
+                 int count=0;
+                 for(int j=0;j<=i;j++){
+                     if(s.charAt(j)==ch){
+                         count++;
+                     }
+                 }
+                System.out.print(count);
+            }
+        }
+            }
+
+
+    }
+
+
