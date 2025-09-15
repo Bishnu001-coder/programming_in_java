72bc868a6cb3f936f9b42bb23374e756b62ed79b
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-09-15 20:08:12 +0530

    solved the BOSSSSS problem

--- src/STRING/_21_print_sub_string_pallindrome_given_string.java
+++ src/STRING/_21_print_sub_string_pallindrome_given_string.java
@@ -7,20 +7,30 @@ public class _21_print_sub_string_pallindrome_given_string {
         if(s.charAt(i)!=s.charAt(j)) return false;
         i++;
         j--;
-    }
+                }
     return true;
-       }
+    }
 
     public static void main(String[] args) {
-    String s="level";
+    String s="level madam bob";
+        String minipal=null;
+        String maxpal="";
     int n=s.length();
 for(int i=0;i<n;i++){
+
     for(int j=i+1;j<=n;j++){
         String sub=s.substring(i,j);
-        if(pallindrome(sub)){
-            System.out.println(sub);
+        if( sub.length()>1 && pallindrome(sub)){
+            if(minipal==null||sub.length()<minipal.length()){
+                minipal=sub;
+            }
+            if(sub.length()>maxpal.length()){
+                maxpal=sub;
+            }
         }
+
     }
 }
+        System.out.println(minipal+"\n"+maxpal);
     }
 }
