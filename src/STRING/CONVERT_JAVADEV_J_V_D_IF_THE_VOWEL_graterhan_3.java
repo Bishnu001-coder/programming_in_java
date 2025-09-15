08e8ee221594aba5800ccee9ce17943252f64ccf
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-09-15 21:52:21 +0530

    solved string if the vowel>=3

---
+++ src/STRING/CONVERT_JAVADEV_J_V_D_IF_THE_VOWEL_graterhan_3.java
@@ -0,0 +1,22 @@
+package STRING;
+
+public class CONVERT_JAVADEV_J_V_D_IF_THE_VOWEL_graterhan_3 {
+    public static void main(String[] args) {
+        String s="javadev";
+        int count=1;
+        char[] ch=s.toCharArray();
+        for(int i=0;i<s.length();i++){
+            if(ch[i]=='a'||ch[i]=='e'||ch[i]=='i'||ch[i]=='o'||ch[i]=='u'){
+                count++;
+                ch[i]=' ';
+
+            }
+
+            }
+        if(count>=3){
+            System.out.println(ch);
+
+        }
+
+    }
+}
