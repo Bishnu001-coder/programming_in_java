f75c0344d97a40540736e91ad8942cbb1c03e66d
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-09-24 21:29:55 +0530

    insertion sort completed

---
+++ src/Sorting/Insertion_sort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class Insertion_sort {
+    public static void insort(int[]a){
+        for(int i=1;i<a.length;i++){
+            int j=i-1;
+            int key=a[i];
+            while(j>=0 && a[j]>key){
+                a[j+1]=a[j];
+                j--;
+            }
+            a[j+1]=key;
+        }
+        }
+
+
+    public static void main(String[] args) {
+        int[] a={9,1,5,2};
+        insort(a);
+        System.out.print(Arrays.toString(a));
+    }
+}
