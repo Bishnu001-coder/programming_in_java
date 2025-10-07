5d7b4a12bfa02cf3e6687c1f40e4c0f2dce7cf01
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-10-07 23:03:41 +0530

    created a class to print reverse the linked list

---
+++ src/_8_DSA/Linked_list/_8_reverse_demo.java
@@ -0,0 +1,13 @@
+package _8_DSA.Linked_list;
+
+public class _8_reverse_demo {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll._2_addlast(1);
+        ll._2_addlast(2);
+        ll._2_addlast(3);
+        ll._4_printll();
+        ll._9_reverse();
+        ll._4_printll();
+    }
+}
