347ff733055f5f58e53d0acede6dc3aa06838195
Bishnu001-coder <bishnumaharana001@gmail.com>
2025-10-07 22:49:39 +0530

    created a add element at last function and printed the add at last element

---
+++ src/_8_DSA/Linked_list/_2_add_last_demo.java
@@ -0,0 +1,14 @@
+package _8_DSA.Linked_list;
+
+public class _2_add_last_demo {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll._4_printll();
+        ll._2_addlast(1);
+        ll._4_printll();
+        ll._2_addlast(2);
+        ll._4_printll();
+        ll._2_addlast(3);
+        ll._4_printll();
+    }
+}
